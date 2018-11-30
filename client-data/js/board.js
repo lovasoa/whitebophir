72e809d57e5f960a2d835872c84a3e7ce65d72e1
Ophir LOJKINE <o.lojkine@qwant.com>
2018-11-30 17:41:34 +0100

    Random color on login!

--- client-data/js/board.js
+++ client-data/js/board.js
@@ -333,6 +333,11 @@ Tools.positionElement = function (elem, x, y) {
 
 Tools.getColor = (function color() {
 	var chooser = document.getElementById("chooseColor");
+	// Init with a random color
+	var clrs = ["#001f3f", "#0074D9", "#7FDBFF", "#39CCCC", "#3D9970",
+		"#2ECC40", "#01FF70", "#FFDC00", "#FF851B", "#FF4136",
+		"#85144b", "#F012BE", "#B10DC9", "#111111", "#AAAAAA"];
+	chooser.value = clrs[Math.random() * clrs.length | 0];
 	return function () { return chooser.value; };
 })();
 
