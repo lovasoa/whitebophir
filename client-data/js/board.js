caf56bd503eb2559c5c3a48ca5f536eb938ae9a4
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2019-01-17 16:48:47 +0100

    Remove will-change optimization after use

--- client-data/js/board.js
+++ client-data/js/board.js
@@ -278,10 +278,16 @@ function updateUnreadCount(m) {
 Tools.messageHooks = [resizeCanvas, updateUnreadCount];
 
 Tools.scale = 1.0;
+var scaleTimeout = null;
 Tools.setScale = function setScale(scale) {
 	if (isNaN(scale)) scale = 1;
 	scale = Math.max(0.1, Math.min(10, scale));
+	Tools.svg.style.willChange = 'transform';
 	Tools.svg.style.transform = 'scale(' + scale + ')';
+	clearTimeout(scaleTimeout);
+	scaleTimeout = setTimeout(function () {
+		Tools.svg.style.willChange = 'auto';
+	}, 1000);
 	Tools.scale = scale;
 	return scale;
 }
