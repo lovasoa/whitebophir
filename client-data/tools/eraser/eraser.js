cba3ea00392f19998eef26c8de8578067cdde0f6
Finn Böger <finnboeger@googlemail.com>
2020-04-26 14:40:42 +0200

    ignore layers on mobile as well

--- client-data/tools/eraser/eraser.js
+++ client-data/tools/eraser/eraser.js
@@ -166,12 +166,13 @@
 						if (elem === null) {
 							//console.error("Eraser: Tried to delete an element that does not exist.");
 						} else {
-							let layer;
+							Tools.drawAndSend(msg);
+							/*let layer;
 							const c = elem.getAttribute("class");
 							if (c && c.startsWith("layer-")) {
 								layer = parseInt(c.substr(6));
 								if (shouldDelete(msg.x, msg.y, layer)) Tools.drawAndSend(msg);
-							}
+							}*/
 						}
 					}
 				}
