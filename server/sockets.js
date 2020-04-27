11ff67bd78de3a0578e53fbc4832f6ff2b8acde9
Finn Böger <finnboeger@googlemail.com>
2020-04-27 12:35:20 +0200

    reduce number of allowed messages

--- server/sockets.js
+++ server/sockets.js
@@ -3,7 +3,7 @@ var iolib = require('socket.io')
 	, BoardData = require("./boardData.js").BoardData;
 
 var MAX_EMIT_COUNT = 200; // Maximum number of draw operations before getting banned
-var MAX_EMIT_COUNT_PERIOD = 1000; // Duration (in ms) after which the emit count is reset
+var MAX_EMIT_COUNT_PERIOD = 5000; // Duration (in ms) after which the emit count is reset
 
 /** Map from name to *promises* of BoardData
 	@type {Object<string, Promise<BoardData>>}
