lovasoa/whitebophir

commit a833ce954923d3c49945f35e76e0b6e979aea5f7

move duplicated cursor creation code to function

client-data/js/board.js
@@ -135,17 +135,21 @@ function handleMarker(evt) {
 
 }
 
+function createCursor(id, color) {
+	var cursor = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	cursor.setAttributeNS(null, "class", "opcursor");
+	cursor.setAttributeNS(null, "id", "mycursor");
+	cursor.setAttributeNS(null, "cx", 0);
+	cursor.setAttributeNS(null, "cy", 0);
+	cursor.setAttributeNS(null, "r", 10);
+	cursor.setAttributeNS(null, "fill", Tools.getColor());
+	return cursor;
+}
 
 function moveMarker(message) {
 	var cursor = Tools.svg.getElementById("mycursor");
 	if(!cursor){
-		cursor = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-		cursor.setAttributeNS(null, "class", "opcursor");
-		cursor.setAttributeNS(null, "id", "mycursor");
-		cursor.setAttributeNS(null, "cx", 0);
-		cursor.setAttributeNS(null, "cy", 0);
-		cursor.setAttributeNS(null, "r", 10);
-		cursor.setAttributeNS(null, "fill", Tools.getColor());
+		cursor = createCursor("mycursor", Tools.getColor());
 		Tools.svg.getElementById("cursors").append(cursor);
 	}
 	cursor.r.baseVal.value=Tools.getSize()/2;
@@ -166,13 +170,7 @@ function moveCursor(message) {
 				cursors[i].setAttributeNS(null, "visibility", "hidden");
 			}
 		}
-		cursor = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-		cursor.setAttributeNS(null, "class", "opcursor");
-		cursor.setAttributeNS(null, "id", "cursor"+message.socket);
-		cursor.setAttributeNS(null, "cx", 0);
-		cursor.setAttributeNS(null, "cy", 0);
-		cursor.setAttributeNS(null, "r", 10);
-		cursor.setAttributeNS(null, "fill", message.c);
+		cursor = createCursor("cursor"+message.socket, message.c);
 		Tools.svg.getElementById("cursors").append(cursor);
 	}
 	cursor.setAttributeNS(null, "fill", message.c);

commit 432cd4d5f9ef916b0de0c3c87f2b0a7f53187c58

fix bug introduced in a833ce9

client-data/js/board.js
@@ -138,11 +138,11 @@ function handleMarker(evt) {
 function createCursor(id, color) {
 	var cursor = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 	cursor.setAttributeNS(null, "class", "opcursor");
-	cursor.setAttributeNS(null, "id", "mycursor");
+	cursor.setAttributeNS(null, "id", id);
 	cursor.setAttributeNS(null, "cx", 0);
 	cursor.setAttributeNS(null, "cy", 0);
 	cursor.setAttributeNS(null, "r", 10);
-	cursor.setAttributeNS(null, "fill", Tools.getColor());
+	cursor.setAttributeNS(null, "fill", color);
 	return cursor;
 }
 