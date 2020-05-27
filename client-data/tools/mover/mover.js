c1a1ff29ff6eddb6594cd60a9393705e546b969c
Paolo Bolzoni <paolo.bolzoni1@studenti.unipr.it>
2020-05-27 23:17:10 +0900

    Add mover skeleton
    Atm it is just the eraser with new icons and renamed variables

---
+++ client-data/tools/mover/mover.js
@@ -0,0 +1,101 @@
+/**
+ *                        WHITEBOPHIR
+ *********************************************************
+ * @licstart  The following is the entire license notice for the 
+ *  JavaScript code in this page.
+ *
+ * Copyright (C) 2013  Ophir LOJKINE
+ *
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GNU GPL) as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.  The code is distributed WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ *
+ * As additional permission under GNU GPL version 3 section 7, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 4, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ *
+ * @licend
+ */
+
+(function mover() { //Code isolation
+
+	var moving = false;
+
+	function startMoving(x, y, evt) {
+		//Prevent the press from being interpreted by the browser
+		evt.preventDefault();
+		moving = true;
+		move(x, y, evt);
+	}
+
+	var msg = {
+		"type": "delete",
+		"id": ""
+	};
+
+	function inDrawingArea(elem) {
+		return Tools.drawingArea.contains(elem);
+	}
+
+	function move(x, y, evt) {
+		// evt.target should be the element over which the mouse is...
+		var target = evt.target;
+		if (evt.type === "touchmove") {
+			// ... the target of touchmove events is the element that was initially touched,
+			// not the one **currently** being touched
+			var touch = evt.touches[0];
+			target = document.elementFromPoint(touch.clientX, touch.clientY);
+		}
+		if (moving && target !== Tools.svg && target !== Tools.drawingArea && inDrawingArea(target)) {
+			msg.id = target.id;
+			Tools.drawAndSend(msg);
+		}
+	}
+
+	function stopMoving() {
+		moving = false;
+	}
+
+	function draw(data) {
+		var elem;
+		switch (data.type) {
+			case "delete":
+				elem = svg.getElementById(data.id);
+				if (elem === null) console.error("Mover: Tried to move an element that does not exist.");
+				else Tools.drawingArea.removeChild(elem);
+				break;
+			default:
+				console.error("Mover: 'move' instruction with unknown type. ", data);
+				break;
+		}
+	}
+
+	var svg = Tools.svg;
+
+	Tools.add({ //The new tool
+		"name": "Mover",
+		"shortcut": "m",
+		"listeners": {
+			"press": startMoving,
+			"move": move,
+			"release": stopMoving,
+		},
+		"secondary": {
+			"name": "Mover-all",
+		    "icon": "tools/mover/icon_all.svg",
+			"active": false
+		},
+		"draw": draw,
+		"icon": "tools/mover/icon_one.svg",
+		"mouseCursor": "move",
+		"showMarker": true,
+	});
+
+})(); //End of code isolation
