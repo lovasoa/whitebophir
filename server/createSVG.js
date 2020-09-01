lovasoa/whitebophir

commit 50da719bda4d1d7d11d8802a047449d5c39bc0dd

Move (#97)
* Add mover skeleton

Atm it is just the eraser with new icons and renamed variables

* The mover not detect the object to move

* We can move ellipses and rectangles

* The mover now moves all types of objects

* Add the move everything functionality

* Aesthetic changes

* Replace the use of const and let with classic var

* Undo few commits

* Start the transform-translate implementation

* Mover now moves with transform translate

* Put the function to get the translation matrix in the Tools

* Shapes (ellipse, line, pencil, rect, and text) now properly load translate coords

* Add the transform-translate to the createSVG functions

* Done movement using transform-translate

* Fix parenthesization bug in the SVG create

* Fix comment about mover states

* Undo translation loading from Ellipse, Mover, Pencil, and Text tools

* Makes the board send update message to the mover as needed

* Remove the old code from the line tool too

* Simplify the mover tool

See #86

* update the mover icon

* Merge the mover and the hand tools

* Improve preview for pre-rendered elements

* v1.6.0

Co-authored-by: Paolo Bolzoni <paolo.bolzoni1@studenti.unipr.it>

server/createSVG.js
@@ -11,7 +11,9 @@ function htmlspecialchars(str) {
 			case '>': return '&gt;';
 			case '&': return '&amp;';
 			case '"': return '&quot;';
-			case "'": return '&#39;'; }});
+			case "'": return '&#39;';
+		}
+	});
 }
 
 function renderPath(el, pathstring) {
@@ -23,6 +25,8 @@ function renderPath(el, pathstring) {
 			('opacity="' + parseFloat(el.opacity) + '" ') : '') +
 		'stroke="' + htmlspecialchars(el.color) + '" ' +
 		'd="' + pathstring + '" ' +
+		(el.deltax || el.deltay ?
+			('transform="translate(' + (+el.deltax) + ',' + (+el.deltay) + ')"') : '') +
 		'/>';
 }
 
@@ -37,6 +41,7 @@ const Tools = {
 			'y="' + (el.y | 0) + '" ' +
 			'font-size="' + (el.size | 0) + '" ' +
 			'fill="' + htmlspecialchars(el.color || "#000") + '" ' +
+			(el.deltax || el.deltay ? ('transform="translate(' + (el.deltax || 0) + ',' + (el.deltay || 0) + ')"') : '') +
 			'>' + htmlspecialchars(el.txt || "") + '</text>';
 	},
 	/**
@@ -65,6 +70,7 @@ const Tools = {
 			'height="' + (el.y2 - el.y) + '" ' +
 			'stroke="' + htmlspecialchars(el.color) + '" ' +
 			'stroke-width="' + (el.size | 0) + '" ' +
+			(el.deltax || el.deltay ? ('transform="translate(' + (el.deltax || 0) + ',' + (el.deltay || 0) + ')"') : '') +
 			'/>';
 	},
 	/**
@@ -100,9 +106,10 @@ async function toSVG(obj, writeable) {
 	const margin = 400;
 	const elems = Object.values(obj);
 	const dim = elems.reduce(function (dim, elem) {
+		if (elem._children) elem = elem._children[0];
 		return [
-			Math.max(elem.x + margin | 0, dim[0]),
-			Math.max(elem.y + margin | 0, dim[1]),
+			Math.max(elem.x + margin + elem.deltax | 0, dim[0]),
+			Math.max(elem.y + margin + elem.deltay | 0, dim[1]),
 		]
 	}, [margin, margin]);
 	writeable.write(

client-data/js/board.js
@@ -314,6 +314,7 @@ Tools.pendingMessages = {};
 function messageForTool(message) {
 	var name = message.tool,
 		tool = Tools.list[name];
+
 	if (tool) {
 		Tools.applyHooks(Tools.messageHooks, message);
 		tool.draw(message, false);
@@ -322,6 +323,11 @@ function messageForTool(message) {
 		//So we add it to the pending messages
 		if (!Tools.pendingMessages[name]) Tools.pendingMessages[name] = [message];
 		else Tools.pendingMessages[name].push(message);
+	}
+
+	if (message.tool !== 'Hand' && message.deltax != null && message.deltay != null) {
+		//this message has special info for the mover
+		messageForTool({ tool: 'Hand', type: 'update', deltax: message.deltax || 0, deltay: message.deltay || 0, id: message.id });
 	}
 }
 
@@ -611,6 +617,7 @@ Tools.getOpacity = (function opacity() {
 		return Math.max(0.1, Math.min(1, chooser.value));
 	};
 })();
+
 
 //Scale the canvas on load
 Tools.svg.width.baseVal.value = document.body.clientWidth;

client-data/tools/hand/hand.js
@@ -1,8 +1,8 @@
 /**
- *                        WHITEBOPHIR
+ *						  WHITEBOPHIR
  *********************************************************
  * @licstart  The following is the entire license notice for the 
- *  JavaScript code in this page.
+ *	JavaScript code in this page.
  *
  * Copyright (C) 2013  Ophir LOJKINE
  *
@@ -24,39 +24,120 @@
  * @licend
  */
 
-(function () { //Code isolation
+(function hand() { //Code isolation
+	var selected = null;
+	var last_sent = 0;
 
-	var orig = { x: 0, y: 0 };
-	var pressed = false;
-	function press(x, y, evt, isTouchEvent) {
+
+	function startMovingElement(x, y, evt) {
+		//Prevent the press from being interpreted by the browser
+		evt.preventDefault();
+		if (!evt.target || !Tools.drawingArea.contains(evt.target)) return;
+		var tmatrix = get_translate_matrix(evt.target);
+		selected = { x: x - tmatrix.e, y: y - tmatrix.f, elem: evt.target };
+	}
+
+	function moveElement(x, y) {
+		if (!selected) return;
+		var deltax = x - selected.x;
+		var deltay = y - selected.y;
+		var msg = { type: "update", id: selected.elem.id, deltax: deltax, deltay: deltay };
+		var now = performance.now();
+		if (now - last_sent > 70) {
+			last_sent = now;
+			Tools.drawAndSend(msg);
+		} else {
+			draw(msg);
+		}
+	}
+
+	function get_translate_matrix(elem) {
+		// Returns the first translate or transform matrix or makes one
+		var translate = null;
+		for (var i = 0; i < elem.transform.baseVal.numberOfItems; ++i) {
+			var baseVal = elem.transform.baseVal[i];
+			// quick tests showed that even if one changes only the fields e and f or uses createSVGTransformFromMatrix
+			// the brower may add a SVG_TRANSFORM_MATRIX instead of a SVG_TRANSFORM_TRANSLATE
+			if (baseVal.type === SVGTransform.SVG_TRANSFORM_TRANSLATE || baseVal.type === SVGTransform.SVG_TRANSFORM_MATRIX) {
+				translate = baseVal;
+				break;
+			}
+		}
+		if (translate == null) {
+			translate = elem.transform.baseVal.createSVGTransformFromMatrix(Tools.svg.createSVGMatrix());
+			elem.transform.baseVal.appendItem(translate);
+		}
+		return translate.matrix;
+	}
+
+	function draw(data) {
+		switch (data.type) {
+			case "update":
+				var elem = Tools.svg.getElementById(data.id);
+				if (!elem) throw new Error("Mover: Tried to move an element that does not exist.");
+				var tmatrix = get_translate_matrix(elem);
+				tmatrix.e = data.deltax || 0;
+				tmatrix.f = data.deltay || 0;
+				break;
+
+			default:
+				throw new Error("Mover: 'move' instruction with unknown type. ", data);
+		}
+	}
+
+	function startHand(x, y, evt, isTouchEvent) {
 		if (!isTouchEvent) {
-			pressed = true;
-			orig.x = document.documentElement.scrollLeft + evt.clientX;
-			orig.y = document.documentElement.scrollTop + evt.clientY;
+			selected = {
+				x: document.documentElement.scrollLeft + evt.clientX,
+				y: document.documentElement.scrollTop + evt.clientY,
+			}
 		}
 	}
-	function move(x, y, evt, isTouchEvent) {
-		if (pressed && !isTouchEvent) { //Let the browser handle touch to scroll
-			window.scrollTo(orig.x - evt.clientX, orig.y - evt.clientY);
+	function moveHand(x, y, evt, isTouchEvent) {
+		if (selected && !isTouchEvent) { //Let the browser handle touch to scroll
+			window.scrollTo(selected.x - evt.clientX, selected.y - evt.clientY);
 		}
-	}
-	function release() {
-		pressed = false;
 	}
 
-	Tools.add({ //The new tool
+	function press(x, y, evt, isTouchEvent) {
+		if (!handTool.secondary.active) startHand(x, y, evt, isTouchEvent);
+		else startMovingElement(x, y, evt, isTouchEvent);
+	}
+
+
+	function move(x, y, evt, isTouchEvent) {
+		if (!handTool.secondary.active) moveHand(x, y, evt, isTouchEvent);
+		else moveElement(x, y, evt, isTouchEvent);
+	}
+
+	function release(x, y, evt, isTouchEvent) {
+		move(x, y, evt, isTouchEvent);
+		selected = null;
+	}
+
+	function switchTool() {
+		selected = null;
+	}
+
+	var handTool = { //The new tool
 		"name": "Hand",
 		"shortcut": "h",
 		"listeners": {
 			"press": press,
 			"move": move,
-			"release": release
+			"release": release,
 		},
-		"icon": "tools/hand/icon.svg",
+		"secondary": {
+			"name": "Mover",
+			"icon": "tools/hand/mover.svg",
+			"active": false,
+			"switch": switchTool,
+		},
+		"draw": draw,
+		"icon": "tools/hand/hand.svg",
 		"mouseCursor": "move",
 		"showMarker": true,
-	});
-
-	//The hand tool is selected by default
-	Tools.change("Hand");
+	};
+	Tools.add(handTool);
+	Tools.change("Hand"); // Use the hand tool by default
 })(); //End of code isolation

client-data/tools/hand/hand.svg
@@ -1,4 +1,4 @@
-<svg viewBox="0 0 72 72" xmlns="http://www.w3.org/2000/svg">
+<svg viewBox="0 0 70 70" xmlns="http://www.w3.org/2000/svg">
     <g fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
         <path d="M18.66 19.24a3.53 3.53 0 10-5.57 4.25l11.54 15.1 2.69 3.39-7.9-10.33a3.53 3.53 0 10-5.56 4.24l7.9 10.34 6.26 7.9c5.47 6.27 14.52 5.93 20.79.46a19.62 19.62 0 006.51-12.31c.39-4.23.81-15.3.81-15.3-.18-2.6-3.13-4.52-3.51-3.18l-4.9 9.76-3.36-4.23 3.36 4.23-3.36-4.23-13.47-17.2a3.53 3.53 0 10-5.56 4.24l4.25 5.57L36 30.42 22.58 12.74a3.53 3.53 0 10-5.56 4.25L31.69 36"/>
         <path stroke-miterlimit="10" d="M11.67 42.87c0 2.57 1.75 4.64 3.9 4.64M7.06 42.44c0 5.6 3.81 10.12 8.52 10.12M45.26 21.24c0-2.57-1.75-4.65-3.9-4.65M49.87 21.67c0-5.6-3.8-10.12-8.51-10.12"/>

client-data/tools/hand/mover.svg
@@ -1,0 +1,10 @@
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70 70" >
+    <path id="arrow" stroke="black" fill="none" stroke-width="2" stroke-linecap="round"
+        d="M 8 28 h 54
+           M 8 42 h 54
+           M 57 22 L 70 35 L 57 48
+        " />
+    <use href="#arrow" transform="rotate(90 35 35)"/>
+    <use href="#arrow" transform="rotate(180 35 35)"/>
+    <use href="#arrow" transform="rotate(-90 35 35)"/>
+</svg>

server/boardData.js
@@ -70,10 +70,13 @@ BoardData.prototype.addChild = function (parentId, child) {
 
 /** Update the data in the board
  * @param {string} id - Identifier of the data to update.
- * @param {object} data - Object containing the the values to update.
+ * @param {object} data - Object containing the values to update.
  * @param {boolean} create - True if the object should be created if it's not currently in the DB.
 */
 BoardData.prototype.update = function (id, data, create) {
+	delete data.type;
+	delete data.tool;
+
 	var obj = this.board[id];
 	if (typeof obj === "object") {
 		for (var i in data) {

server/sockets.js
@@ -140,7 +140,6 @@ async function saveHistory(boardName, message) {
 			if (id) board.delete(id);
 			break;
 		case "update":
-			delete message.type;
 			if (id) board.update(id, message);
 			break;
 		case "child":

commit 8c001ca0e88e1c4633ec54012896506ff404b2e5

Fix board preview
A bug was introduced in 50da719bda4d1d7d11d8802a047449d5c39bc0dd
most elements do not have a deltax and deltay property

See #97

server/createSVG.js
@@ -108,8 +108,8 @@ async function toSVG(obj, writeable) {
 	const dim = elems.reduce(function (dim, elem) {
 		if (elem._children) elem = elem._children[0];
 		return [
-			Math.max(elem.x + margin + elem.deltax | 0, dim[0]),
-			Math.max(elem.y + margin + elem.deltay | 0, dim[1]),
+			Math.max(elem.x + margin + (elem.deltax | 0) | 0, dim[0]),
+			Math.max(elem.y + margin + (elem.deltay | 0) | 0, dim[1]),
 		]
 	}, [margin, margin]);
 	writeable.write(