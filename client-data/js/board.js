693067d498a442878d9b7c82a460c7984622999a
David Roundy <roundyd@physics.oregonstate.edu>
2020-09-25 06:26:42 -0700

    Fix tabs/spaces.
    Co-authored-by: Ophir LOJKINE <pere.jobs@gmail.com>

--- client-data/js/board.js
+++ client-data/js/board.js
@@ -415,7 +415,7 @@ function resizeCanvas(m) {
 	//Enlarge the canvas whenever something is drawn near its border
 	var x = m.x | 0, y = m.y | 0
 	if (x > Tools.svg.width.baseVal.value - 2000) {
-        Tools.svg.width.baseVal.value = Math.min(x + 2000, Tools.server_config.MAX_BOARD_SIZE_X);
+		Tools.svg.width.baseVal.value = Math.min(x + 2000, Tools.server_config.MAX_BOARD_SIZE_X);
 	}
 	if (y > Tools.svg.height.baseVal.value - 2000) {
 		Tools.svg.height.baseVal.value = Math.min(y + 2000, Tools.server_config.MAX_BOARD_SIZE);
