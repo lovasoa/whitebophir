53c61ec16e07d31a4795a429171c4e63d7ec4b8f
lovasoa <pere.jobs@gmail.com>
2021-02-07 19:02:56 +0100

    Reformat all the server code with prettier

--- server/check_output_directory.js
+++ server/check_output_directory.js
@@ -1,6 +1,6 @@
 const fs = require("./fs_promises");
-const path = require('path');
-let os = require('os');
+const path = require("path");
+let os = require("os");
 
 const { R_OK, W_OK } = fs.constants;
 
@@ -10,56 +10,66 @@ const { R_OK, W_OK } = fs.constants;
  * @returns {string?}
  */
 async function get_error(directory) {
-    if (!fs.existsSync(directory)) {
-        return "does not exist";
+  if (!fs.existsSync(directory)) {
+    return "does not exist";
+  }
+  if (!fs.statSync(directory).isDirectory()) {
+    error = "exists, but is not a directory";
+  }
+  const { uid, gid } = os.userInfo();
+  const tmpfile = path.join(directory, Math.random() + ".json");
+  try {
+    fs.writeFileSync(tmpfile, "{}");
+    fs.unlinkSync(tmpfile);
+  } catch (e) {
+    return (
+      "does not allow file creation and deletion. " +
+      "Check the permissions of the directory, and if needed change them so that " +
+      `user with UID ${uid} has access to them. This can be achieved by running the command: chown ${uid}:${gid} on the directory`
+    );
+  }
+  const fileChecks = [];
+  const files = await fs.promises.readdir(directory, { withFileTypes: true });
+  for (const elem of files) {
+    if (/^board-(.*)\.json$/.test(elem.name)) {
+      const elemPath = path.join(directory, elem.name);
+      if (!elem.isFile())
+        return `contains a board file named "${elemPath}" which is not a normal file`;
+      fileChecks.push(
+        fs.promises.access(elemPath, R_OK | W_OK).catch(function () {
+          return elemPath;
+        })
+      );
     }
-    if (!fs.statSync(directory).isDirectory()) {
-        error = "exists, but is not a directory";
-    }
-    const { uid, gid } = os.userInfo();
-    const tmpfile = path.join(directory, Math.random() + ".json");
-    try {
-        fs.writeFileSync(tmpfile, "{}");
-        fs.unlinkSync(tmpfile);
-    } catch (e) {
-        return "does not allow file creation and deletion. " +
-            "Check the permissions of the directory, and if needed change them so that " +
-            `user with UID ${uid} has access to them. This can be achieved by running the command: chown ${uid}:${gid} on the directory`;
-    }
-    const fileChecks = [];
-    const files = await fs.promises.readdir(directory, {withFileTypes: true});
-    for (const elem of files) {
-        if (/^board-(.*)\.json$/.test(elem.name)) {
-            const elemPath = path.join(directory, elem.name);
-            if (!elem.isFile()) return `contains a board file named "${elemPath}" which is not a normal file`
-            fileChecks.push(fs.promises.access(elemPath, R_OK | W_OK)
-                .catch(function () { return elemPath }))
-        }
-    }
-    const errs = (await Promise.all(fileChecks)).filter(function (x) { return x });
-    if (errs.length > 0) {
-        return `contains the following board files that are not readable and writable by the current user: "` +
-            errs.join('", "') +
-            `". Please make all board files accessible with chown 1000:1000`
-    }
+  }
+  const errs = (await Promise.all(fileChecks)).filter(function (x) {
+    return x;
+  });
+  if (errs.length > 0) {
+    return (
+      `contains the following board files that are not readable and writable by the current user: "` +
+      errs.join('", "') +
+      `". Please make all board files accessible with chown 1000:1000`
+    );
+  }
 }
 
 /**
- * Checks that the output directory is writeable, 
+ * Checks that the output directory is writeable,
  * ans exits the current process with an error otherwise.
  * @param {string} directory
  */
 function check_output_directory(directory) {
-    get_error(directory).then(function (error) {
-        if (error) {
-            console.error(
-                `The configured history directory in which boards are stored ${error}.` +
-                `\nThe history directory can be configured with the environment variable HISTORY_DIR. ` +
-                `It is currently set to "${directory}".`
-            );
-            process.exit(1);
-        }
-    })
+  get_error(directory).then(function (error) {
+    if (error) {
+      console.error(
+        `The configured history directory in which boards are stored ${error}.` +
+          `\nThe history directory can be configured with the environment variable HISTORY_DIR. ` +
+          `It is currently set to "${directory}".`
+      );
+      process.exit(1);
+    }
+  });
 }
 
-module.exports = check_output_directory
+module.exports = check_output_directory;
--- server/configuration.js
+++ server/configuration.js
@@ -2,41 +2,43 @@ const path = require("path");
 const app_root = path.dirname(__dirname); // Parent of the directory where this file is
 
 module.exports = {
-    /** Port on which the application will listen */
-    PORT: parseInt(process.env['PORT']) || 8080,
+  /** Port on which the application will listen */
+  PORT: parseInt(process.env["PORT"]) || 8080,
 
-    /** Host on which the application will listen (defaults to undefined,
+  /** Host on which the application will listen (defaults to undefined,
         hence listen on all interfaces on all IP addresses, but could also be
         '127.0.0.1' **/
-    HOST: process.env['HOST'] || undefined,
+  HOST: process.env["HOST"] || undefined,
 
-    /** Path to the directory where boards will be saved by default */
-    HISTORY_DIR: process.env['WBO_HISTORY_DIR'] || path.join(app_root, "server-data"),
+  /** Path to the directory where boards will be saved by default */
+  HISTORY_DIR:
+    process.env["WBO_HISTORY_DIR"] || path.join(app_root, "server-data"),
 
-    /** Folder from which static files will be served */
-    WEBROOT: process.env['WBO_WEBROOT'] || path.join(app_root, "client-data"),
+  /** Folder from which static files will be served */
+  WEBROOT: process.env["WBO_WEBROOT"] || path.join(app_root, "client-data"),
 
-    /** Number of milliseconds of inactivity after which the board should be saved to a file */
-    SAVE_INTERVAL: parseInt(process.env['WBO_SAVE_INTERVAL']) || 1000 * 2, // Save after 2 seconds of inactivity
+  /** Number of milliseconds of inactivity after which the board should be saved to a file */
+  SAVE_INTERVAL: parseInt(process.env["WBO_SAVE_INTERVAL"]) || 1000 * 2, // Save after 2 seconds of inactivity
 
-    /** Periodicity at which the board should be saved when it is being actively used (milliseconds)  */
-    MAX_SAVE_DELAY: parseInt(process.env['WBO_MAX_SAVE_DELAY']) || 1000 * 60, // Save after 60 seconds even if there is still activity
+  /** Periodicity at which the board should be saved when it is being actively used (milliseconds)  */
+  MAX_SAVE_DELAY: parseInt(process.env["WBO_MAX_SAVE_DELAY"]) || 1000 * 60, // Save after 60 seconds even if there is still activity
 
-    /** Maximal number of items to keep in the board. When there are more items, the oldest ones are deleted */
-    MAX_ITEM_COUNT: parseInt(process.env['WBO_MAX_ITEM_COUNT']) || 32768,
+  /** Maximal number of items to keep in the board. When there are more items, the oldest ones are deleted */
+  MAX_ITEM_COUNT: parseInt(process.env["WBO_MAX_ITEM_COUNT"]) || 32768,
 
-    /** Max number of sub-items in an item. This prevents flooding */
-    MAX_CHILDREN: parseInt(process.env['WBO_MAX_CHILDREN']) || 192,
+  /** Max number of sub-items in an item. This prevents flooding */
+  MAX_CHILDREN: parseInt(process.env["WBO_MAX_CHILDREN"]) || 192,
 
-    /** Maximum value for any x or y on the board */
-    MAX_BOARD_SIZE: parseInt(process.env['WBO_MAX_BOARD_SIZE']) || 65536,
+  /** Maximum value for any x or y on the board */
+  MAX_BOARD_SIZE: parseInt(process.env["WBO_MAX_BOARD_SIZE"]) || 65536,
 
-    /** Maximum messages per user over the given time period before banning them  */
-    MAX_EMIT_COUNT: parseInt(process.env['WBO_MAX_EMIT_COUNT']) || 192,
+  /** Maximum messages per user over the given time period before banning them  */
+  MAX_EMIT_COUNT: parseInt(process.env["WBO_MAX_EMIT_COUNT"]) || 192,
 
-    /** Duration after which the emit count is reset in miliseconds */
-    MAX_EMIT_COUNT_PERIOD: parseInt(process.env['WBO_MAX_EMIT_COUNT_PERIOD']) || 4096,
+  /** Duration after which the emit count is reset in miliseconds */
+  MAX_EMIT_COUNT_PERIOD:
+    parseInt(process.env["WBO_MAX_EMIT_COUNT_PERIOD"]) || 4096,
 
-    /** Blocked Tools. A comma-separated list of tools that should not appear on boards. */
-    BLOCKED_TOOLS: (process.env['WBO_BLOCKED_TOOLS'] || "").split(','),
+  /** Blocked Tools. A comma-separated list of tools that should not appear on boards. */
+  BLOCKED_TOOLS: (process.env["WBO_BLOCKED_TOOLS"] || "").split(","),
 };
