9330bd4a37d730c9bb0d31e4e649e2ae87227127
Russ Tedrake <russt@mit.edu>
2021-02-16 21:18:00 -0500

    Adds support for query parameter "hideMenu"
    This adds the ability to load a board with the menu hidden; it provides a form of "read-only" mode, but the key bindings are still active.
    Use with, e.g.:
    http://localhost:5001/boards/qYAZyGdym6HqjnfaGI4yNtpdx0IMHd9qp6kaJPwbJqE-?hideMenu
    
    The alternative, proposed in #116, was to remove the menu completely, but this would require more substantial changes to board.js (which hangs on "Loading..." if the menu is removed).
    Related to #150
    Related to #116

--- server/templating.js
+++ server/templating.js
@@ -68,6 +68,7 @@ class BoardTemplate extends Template {
     const boardUriComponent = parts[1];
     params["boardUriComponent"] = boardUriComponent;
     params["board"] = decodeURIComponent(boardUriComponent);
+    params["hideMenu"] = parsedUrl.search.includes("hideMenu");
     return params;
   }
 }
