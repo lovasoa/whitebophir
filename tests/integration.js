9fb0d9687cdc226b796489e4d10339ec356ac84e
Ophir LOJKINE <pere.jobs@gmail.com>
2023-10-23 19:20:52 +0200

    longer timeout

--- tests/integration.js
+++ tests/integration.js
@@ -76,7 +76,7 @@ function testCircle(browser) {
         })
         .assert.visible("ellipse[cx='0'][cy='200'][rx='200'][ry='200'][stroke='#112233']")
         .refresh()
-        .waitForElementVisible("ellipse[cx='0'][cy='200'][rx='200'][ry='200'][stroke='#112233']")
+        .waitForElementVisible("ellipse[cx='0'][cy='200'][rx='200'][ry='200'][stroke='#112233']", 15000)
         .click('#toolID-Ellipse') // Click the ellipse tool
         .click('#toolID-Ellipse') // Click again to toggle
         .assert.containsText('#toolID-Ellipse .tool-name', 'Cercle') // Circle in french
