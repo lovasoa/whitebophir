7da292a601b12a78502e0ac37181d132d4191ab9
bhavik001 <bhmistry@myseneca.ca>
2023-12-11 00:49:50 -0500

    Resolving build error

--- server/check_output_directory.js
+++ server/check_output_directory.js
@@ -29,8 +29,9 @@ async function get_error(directory) {
         "Check the permissions of the directory, and if needed change them so that " +
         `user with UID ${uid} has access to them. This can be achieved by running the command: chown ${uid}:${gid} on the directory`;
     } finally {
-      console.log(err_msg);
-      // return err_msg;
+      // console.log(err_msg);
+      // eslint-disable-next-line no-unsafe-finally
+      return err_msg;
     }
   }
   const fileChecks = [];
