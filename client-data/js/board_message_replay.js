80656fd04f87d43a8b9068511a283848810157fc
Ophir LOJKINE <ophir.lojkine@datadoghq.com>
2026-04-15 00:00:58 +0200

    Unwrap board message replay module

--- client-data/js/board_message_replay.js
+++ client-data/js/board_message_replay.js
@@ -1,105 +1,109 @@
-(function (root, factory) {
-  var api = factory();
-  if (typeof module === "object" && module.exports) module.exports = api;
-  /** @type {any} */ (root).WBOBoardMessageReplay = api;
-})(typeof globalThis !== "undefined" ? globalThis : this, function () {
-  /** @type {{[toolName: string]: true}} */
-  var TOOL_OWNED_BATCH_TOOLS = {
-    Hand: true,
-  };
+/** @type {{[toolName: string]: true}} */
+var TOOL_OWNED_BATCH_TOOLS = {
+  Hand: true,
+};
 
-  /**
-   * @param {unknown} value
-   * @returns {number}
-   */
-  function normalizeRevision(value) {
-    var revision = Number(value);
-    return Number.isSafeInteger(revision) && revision > 0 ? revision : 0;
+/**
+ * @param {unknown} value
+ * @returns {number}
+ */
+function normalizeRevision(value) {
+  var revision = Number(value);
+  return Number.isSafeInteger(revision) && revision > 0 ? revision : 0;
+}
+
+/**
+ * @param {{_children?: unknown}} message
+ * @returns {boolean}
+ */
+function hasArrayChildren(message) {
+  return !!(message && Array.isArray(message._children));
+}
+
+/**
+ * @param {{tool?: unknown, _children?: unknown}} message
+ * @returns {boolean}
+ */
+function isSnapshotMessage(message) {
+  return hasArrayChildren(message) && !message.tool;
+}
+
+/**
+ * @param {{tool?: unknown, _children?: unknown}} message
+ * @returns {boolean}
+ */
+function isToolOwnedBatchMessage(message) {
+  return !!(
+    hasArrayChildren(message) &&
+    typeof message.tool === "string" &&
+    TOOL_OWNED_BATCH_TOOLS[message.tool] === true
+  );
+}
+
+/**
+ * @param {{tool?: unknown, _children?: unknown}} message
+ * @returns {boolean}
+ */
+function shouldReplayChildrenIndividually(message) {
+  return hasArrayChildren(message) && !isToolOwnedBatchMessage(message);
+}
+
+/**
+ * @template T
+ * @param {{id?: unknown, tool?: unknown, _children?: unknown}} parent
+ * @param {T} child
+ * @param {(parent: any, child: T) => T} normalizeChildMessage
+ * @returns {T}
+ */
+function prepareReplayChild(parent, child, normalizeChildMessage) {
+  if (parent && typeof parent.id === "string" && parent.id !== "") {
+    return normalizeChildMessage(parent, child);
   }
+  return child;
+}
 
-  /**
-   * @param {{_children?: unknown}} message
-   * @returns {boolean}
-   */
-  function hasArrayChildren(message) {
-    return !!(message && Array.isArray(message._children));
-  }
+/**
+ * @param {{tool?: unknown, _children?: unknown} | null | undefined} message
+ * @param {boolean} awaitingBoardSnapshot
+ * @returns {boolean}
+ */
+function shouldBufferLiveMessage(message, awaitingBoardSnapshot) {
+  return awaitingBoardSnapshot === true && !isSnapshotMessage(message || {});
+}
 
-  /**
-   * @param {{tool?: unknown, _children?: unknown}} message
-   * @returns {boolean}
-   */
-  function isSnapshotMessage(message) {
-    return hasArrayChildren(message) && !message.tool;
-  }
-
-  /**
-   * @param {{tool?: unknown, _children?: unknown}} message
-   * @returns {boolean}
-   */
-  function isToolOwnedBatchMessage(message) {
-    return !!(
-      hasArrayChildren(message) &&
-      typeof message.tool === "string" &&
-      TOOL_OWNED_BATCH_TOOLS[message.tool] === true
+/**
+ * @template {{revision?: unknown}} T
+ * @param {T[]} messages
+ * @param {unknown} snapshotRevision
+ * @returns {T[]}
+ */
+function filterBufferedMessagesAfterSnapshot(messages, snapshotRevision) {
+  var normalizedSnapshotRevision = normalizeRevision(snapshotRevision);
+  return messages.filter(function (message) {
+    var messageRevision = normalizeRevision(message && message.revision);
+    return (
+      messageRevision === 0 || messageRevision > normalizedSnapshotRevision
     );
-  }
+  });
+}
 
-  /**
-   * @param {{tool?: unknown, _children?: unknown}} message
-   * @returns {boolean}
-   */
-  function shouldReplayChildrenIndividually(message) {
-    return hasArrayChildren(message) && !isToolOwnedBatchMessage(message);
-  }
+var boardMessageReplay = {
+  TOOL_OWNED_BATCH_TOOLS: TOOL_OWNED_BATCH_TOOLS,
+  filterBufferedMessagesAfterSnapshot: filterBufferedMessagesAfterSnapshot,
+  isSnapshotMessage: isSnapshotMessage,
+  isToolOwnedBatchMessage: isToolOwnedBatchMessage,
+  normalizeRevision: normalizeRevision,
+  prepareReplayChild: prepareReplayChild,
+  shouldBufferLiveMessage: shouldBufferLiveMessage,
+  shouldReplayChildrenIndividually: shouldReplayChildrenIndividually,
+};
 
-  /**
-   * @template T
-   * @param {{id?: unknown, tool?: unknown, _children?: unknown}} parent
-   * @param {T} child
-   * @param {(parent: any, child: T) => T} normalizeChildMessage
-   * @returns {T}
-   */
-  function prepareReplayChild(parent, child, normalizeChildMessage) {
-    if (parent && typeof parent.id === "string" && parent.id !== "") {
-      return normalizeChildMessage(parent, child);
-    }
-    return child;
-  }
+var root = /** @type {typeof globalThis & {
+    WBOBoardMessageReplay?: typeof boardMessageReplay,
+  }} */ (typeof globalThis === "object" ? globalThis : window);
 
-  /**
-   * @param {{tool?: unknown, _children?: unknown} | null | undefined} message
-   * @param {boolean} awaitingBoardSnapshot
-   * @returns {boolean}
-   */
-  function shouldBufferLiveMessage(message, awaitingBoardSnapshot) {
-    return awaitingBoardSnapshot === true && !isSnapshotMessage(message || {});
-  }
+root.WBOBoardMessageReplay = boardMessageReplay;
 
-  /**
-   * @template {{revision?: unknown}} T
-   * @param {T[]} messages
-   * @param {unknown} snapshotRevision
-   * @returns {T[]}
-   */
-  function filterBufferedMessagesAfterSnapshot(messages, snapshotRevision) {
-    var normalizedSnapshotRevision = normalizeRevision(snapshotRevision);
-    return messages.filter(function (message) {
-      var messageRevision = normalizeRevision(message && message.revision);
-      return (
-        messageRevision === 0 || messageRevision > normalizedSnapshotRevision
-      );
-    });
-  }
-
-  return {
-    TOOL_OWNED_BATCH_TOOLS: TOOL_OWNED_BATCH_TOOLS,
-    filterBufferedMessagesAfterSnapshot: filterBufferedMessagesAfterSnapshot,
-    isSnapshotMessage: isSnapshotMessage,
-    isToolOwnedBatchMessage: isToolOwnedBatchMessage,
-    normalizeRevision: normalizeRevision,
-    prepareReplayChild: prepareReplayChild,
-    shouldBufferLiveMessage: shouldBufferLiveMessage,
-    shouldReplayChildrenIndividually: shouldReplayChildrenIndividually,
-  };
-});
+if ("object" === typeof module && module.exports) {
+  module.exports = boardMessageReplay;
+}
