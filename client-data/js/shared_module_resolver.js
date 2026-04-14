824d0447172f9c6b288519c548e1fd14be3f1460
Ophir LOJKINE <ophir.lojkine@datadoghq.com>
2026-04-15 00:18:22 +0200

    Unwrap shared module metadata helpers

--- client-data/js/shared_module_resolver.js
+++ client-data/js/shared_module_resolver.js
@@ -1,31 +1,33 @@
-((root, factory) => {
-  var api = factory();
-  if (typeof module === "object" && module.exports) module.exports = api;
-  /** @type {any} */ (root).WBOSharedModuleResolver = api;
-})(typeof globalThis !== "undefined" ? globalThis : this, () => {
-  /**
-   * @param {string} requirePath
-   * @param {string} globalName
-   * @param {any} [scope]
-   * @returns {any}
-   */
-  function resolveSharedModule(requirePath, globalName, scope) {
-    if (
-      typeof module === "object" &&
-      module.exports &&
-      typeof require === "function"
-    ) {
-      return require(requirePath);
-    }
-    var globalScope =
-      scope ||
-      (typeof globalThis !== "undefined"
-        ? globalThis
-        : /** @type {any} */ ({}));
-    return globalScope[globalName] || null;
+/**
+ * @param {string} requirePath
+ * @param {string} globalName
+ * @param {any} [scope]
+ * @returns {any}
+ */
+function resolveSharedModule(requirePath, globalName, scope) {
+  if (
+    typeof module === "object" &&
+    module.exports &&
+    typeof require === "function"
+  ) {
+    return require(requirePath);
   }
+  var globalScope =
+    scope ||
+    (typeof globalThis !== "undefined" ? globalThis : /** @type {any} */ ({}));
+  return globalScope[globalName] || null;
+}
 
-  return {
-    resolveSharedModule: resolveSharedModule,
-  };
-});
+var sharedModuleResolver = {
+  resolveSharedModule: resolveSharedModule,
+};
+
+var root = /** @type {typeof globalThis & {
+    WBOSharedModuleResolver?: typeof sharedModuleResolver,
+  }} */ (typeof globalThis !== "undefined" ? globalThis : this);
+
+root.WBOSharedModuleResolver = sharedModuleResolver;
+
+if (typeof module === "object" && module.exports) {
+  module.exports = sharedModuleResolver;
+}
